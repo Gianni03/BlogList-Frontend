71d0b0129cdf3d68254997157d6fa5f892aa1ed1
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2026-03-01 16:42:39 -0300

    feat: Implement user login/logout, session management, blog creation, and notifications.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,140 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from 'react';
+import Blog from './components/Blog';
+import Notification from './components/Notification';
+import blogService from './services/blogs';
+import loginService from './services/login';
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser');
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
+    }
+  }, []);
+
+  const addBlog = async (event) => {
+    event.preventDefault();
+
+    const newBlog = await blogService.create({
+      title,
+      author,
+      url,
+    });
+
+    setBlogs(blogs.concat(newBlog));
+
+    setMessage(`a new blog ${newBlog.title} added`);
+    setTimeout(() => {
+      setMessage(null);
+    }, 5000);
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      console.log('login exitoso', user);
+
+      setUser(user);
+      setUsername('');
+      setPassword('');
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user));
+      blogService.setToken(user.token);
+      setMessage(`Welcome ${user.name}`)
+    } catch (exception) {
+      setMessage('wrong username or password');
+      setTimeout(() => {
+        setMessage(null);
+      }, 5000);
+    }
+  };
+
+  const handleLogout = () => {
+    window.localStorage.removeItem('loggedBlogappUser');
+    setUser(null);
+  };
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Notification message={message} />
+
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    );
+  }
 
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+      <Notification message={message} />
+      <p>
+        {user.name} logged in
+        <button onClick={handleLogout}>logout</button>
+      </p>
+      <form onSubmit={addBlog}>
+        <input
+          placeholder="title"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
+        />
+        <input
+          placeholder="author"
+          value={author}
+          onChange={({ target }) => setAuthor(target.value)}
+        />
+        <input
+          placeholder="url"
+          value={url}
+          onChange={({ target }) => setUrl(target.value)}
+        />
+        <button type="submit">create</button>
+      </form>
 
-export default App
+      {blogs.map((blog) => (
+        <div key={blog.id}>
+          {blog.title} {blog.author}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
